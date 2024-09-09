9facd671ac052b9ec53e5f5af8ddabd1049edd00
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-09-09 23:41:32 +0530

    added the header components

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='shadow-sm border-b sticky top-0 z-30 bg-white'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto'>
+            {/* logo */}
+
+            <Link className="hidden lg:inline-flex" href="/">
+                    <Image src="/insta-logo.png" alt='insta-logo' height={96} width={96} />
+            </Link>
+            <Link className='lg:hidden' href="/">
+                    <Image src="/logo.png" alt='logo' height={40} width={40} />
+            </Link>
+
+            {/* Search Items */}
+
+            <input type="text" placeholder='Search'
+                className='bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[300px]'
+            />
+
+            {/* menu items */}
+
+            <button className='text-sm semi-bold text-blue-500'>Log In</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
